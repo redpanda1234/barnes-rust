83086004cf82e0b26830d70782fc9e9dc6e458ae
redpanda1234 <fkobayashi@hmc.edu>
2017-11-04 01:41:56 -0700

    did dumb stuff

--- src/simulation/tree.rs
+++ src/simulation/tree.rs
@@ -1,15 +1,12 @@
-extern crate num_cpus; // So that we can know how many processors to work with
 use std::thread;       // For fearless concurrency
-
 
 // Static -> valid globally throughout the lifetime of the program
 // mut allows us to modify the value contained in the static
-static mut NUM_THREADS: i64 = 20; // magic number for now, will find more
-                                  // intelligent thread spawner later
+static mut NUM_THREADS: i64 = 20; // TODO: intelligent thread limit
 
 #[derive(Debug)] // for printing stuff
 pub struct Region {
-    pub reg_vec: Option<Vec<Option<Region>>> // FIXME: make this syntactically valid
+    pub reg_vec: Option<Vec<Option<Region>>>,
 
     /* Dead code?
     // pub ne: Option<region>, // Option enum; either some or none.
@@ -26,7 +23,6 @@ pub struct Region {
     pub add_bucket: Option<Vec<Coord>>, // masses to inject
 
     pub com: Option<Coord>,
-
 }
 
 pub struct Coord {
@@ -46,20 +42,32 @@ impl Region {
     // contains takes a reference to the self struct and a point
     // struct, then determines whether point is contained within
     // the bounds of region.
+
+    // TODO: implement dropping of dead branches
+
+    // TODO: collisions
+
+    // Also: really really close bodies merge, but add the bonding
+    // energy term
+
     fn contains(&self, point: &Point) -> bool {
-        if point.x <= self.x + self.length && point.x >= self.x {
-            // Since rust is an expression language thing, then the
-            // last evaluated exp (below, a bool) will be the return
-            // value of the function
+
+        // Since rust is an expression language thing, then the
+        // last evaluated exp (below, a bool) will be the return
+        // value of the function
+        point.x <= self.x + self.length && point.x >= self.x &&
             point.y <= self.y + self.length && point.y >= self.y
-        }
-        // else return false
-        false
     }
 
-    fn calc_com(&self) -> Coord {
-        if remove {
-            self.com = None
+    fn update(&self) -> Coord {
+        match self.reg_vec {
+            None => {
+                if
+            }
+
+            Some() => {
+
+            }
         }
     }
 }
