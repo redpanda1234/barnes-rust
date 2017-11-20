078c2d1448e18b2d8e3e870724a05739367b1d8a
redpanda1234 <fkobayashi@hmc.edu>
2017-11-20 02:36:40 -0800

    Add distance metric

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ mod tree;
 
 pub use physics::*;
 
-
+// TODO: use this everywhere we check dimensions
+pub const DIMS: usize = 2;
 
 fn main() {
     // let mut static MULTIPLIERS
--- src/physics.rs
+++ src/physics.rs
@@ -1,6 +1,8 @@
 pub use super::tree::*;
+pub use super::DIMS;
 
 static THETA: f64 = 0.5;
+
 
 impl Body {
     fn squared_dist_to(&self, mass: &Body) -> f64 {
@@ -41,11 +43,39 @@ impl Body {
         // this makes me think we should store full-length instead of
         // half-length
         match node.com {
-            None => {node.get_com(); self.is_far(node)}
+            None => {node.add_com(); self.is_far(node)},
             Some(com) => {
                 2.0 * node.half_length /
                     (self.squared_dist_to(&node.com.clone().unwrap()))
                     <= THETA
+            },
+        }
+    }
+}
+
+impl Region {
+    fn calc_com(&self) -> Body {
+        match self.reg_vec {
+            None => self.com.clone().unwrap(),
+            // This assumes we've pruned dead children, which we
+            // haven't quite done yet.
+            Some(ref reg_vec) => {
+                let mut num = vec![0.0; DIMS as usize];
+                let mut den = 0.0;
+
+                for child in self.reg_vec.clone().unwrap().iter() {
+                    den += child.calc_com().mass;
+                    let com = child.com.clone().unwrap();
+                    // vec = self.pos_vec.clone()
+                    for i in 0..DIMS {
+                        num[i] += com.pos_vec[i] * com.mass;
+                    }
+                }
+                for i in 0..DIMS {
+                    num[i] /= den
+                }
+                Body {pos_vec: num, vel_vec: vec![0.0; DIMS as usize],
+                    mass: den}
             }
         }
     }
