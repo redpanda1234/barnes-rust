0484c36ffbd2cba0aaeefa8ece102ff96f91a8d4
hfetsch <harryfetsch@gmail.com>
2017-12-07 23:05:38 -0800

    make rectangles display in red

--- src/gfx.rs
+++ src/gfx.rs
@@ -63,6 +63,7 @@ impl Pixel {
 
         const BLACK: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
         const WHITE: [f32; 4] = [0.0, 0.0, 0.0, 0.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 0.0];
 
         let square = rectangle::square(0.0, 0.0, 1080.0);
     }
@@ -76,9 +77,12 @@ impl Frame {
 
         const BLACK: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
         const WHITE: [f32; 4] = [0.0, 0.0, 0.0, 0.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 0.0];
 
         let square = rectangle::square(0.0, 0.0, 1080.0);
 
+        //main should pass render() a None option
+        //if that happens, call render on the tree
         match reg_option {
             None => {
                 let tree = self.tree.clone();
@@ -97,7 +101,7 @@ impl Frame {
                             let transform = c.transform.trans(coords[0], coords[1])
                                             .rot_rad(0.0)
                                             .trans(-25.0, -25.0);
-                            rectangle(WHITE, square, transform, gl);
+                            rectangle(RED, square, transform, gl);
                         });
                     },
 
