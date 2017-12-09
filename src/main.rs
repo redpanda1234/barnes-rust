4d2f9a53efec419901f7364fbc3ad328ffc36fba
redpanda1234 <fkobayashi@hmc.edu>
2017-12-08 23:17:46 -0800

    begin implementing with arc and mutex

--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,7 @@ fn main() {
     // it gives us reproducible results.
     let num_bodies = 100;
 
-    generate::gt_all_ranges(num_bodies);
+    let root = generate::gt_all_ranges(num_bodies);
 
     let mut frame = Frame {
         gl: GlGraphics::new(opengl),
